dd8b7b2547b22e0a9f569c24282705b380c1cb0c
Ricardo Zancan <ricardozancanc@gmail.com>
2024-09-12 22:59:36 -0300

    Holiday init

---
+++ lib/view/holiday_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HolidayPage extends StatefulWidget {
+  const HolidayPage({super.key});
+
+  @override
+  State<HolidayPage> createState() => _HolidayPageState();
+}
+
+class _HolidayPageState extends State<HolidayPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/view/menu.dart
+++ lib/view/menu.dart
@@ -1,3 +1,4 @@
+import 'package:appadsgp1/view/home_page.dart';
 import 'package:flutter/material.dart';
 
 class MenuDrawer extends StatelessWidget {
@@ -16,6 +17,12 @@ class MenuDrawer extends StatelessWidget {
               title: Text("Cursos"),
               onTap: () {
                 //navegar para uma nova tela
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomePage(),
+                  ),
+                ).then((value) => Navigator.of(context).pop());
               },
             ),
             ListTile(
